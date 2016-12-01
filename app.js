1d1f206ef4185314d44d9f0642497fddc0c45080
Rod Reyes <roderick.reyes@gmail.com>
2016-12-01 17:01:45 +0100

    install mongoose

--- app.js
+++ app.js
@@ -20,18 +20,6 @@ mongodb.connect(db_url, function (err, db) {
   if (err) console.error('error connecting: ' + err.stack);
   else console.log('Connection established to', db_url);
 
-  const collection = db.collection('documents');
-  // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-
   app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
   });
